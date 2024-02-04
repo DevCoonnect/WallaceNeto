e495e644bdb59f35c73a0b373bc6bbd8b42a3b0c
Wallace Neto <wallace.neto@outlook.com>
2024-02-04 20:07:23 -0300

    creating drawer header

--- Tasks/src/components/Drawer/CustomDrawer.tsx
+++ Tasks/src/components/Drawer/CustomDrawer.tsx
@@ -3,15 +3,30 @@ import {
   DrawerContentScrollView,
   DrawerItemList,
 } from '@react-navigation/drawer';
-import {View, Text} from 'react-native';
+import {Text, View} from 'react-native';
+import Gravatar from '@krosben/react-native-gravatar';
 
 import styles from './styles';
 
 export default function CustomDrawer(props: any) {
+  const name = 'Teste da Silva';
+  const email = 'teste@teste.com';
+
   return (
     <DrawerContentScrollView {...props}>
       <View style={styles.header}>
-        <Text>Header</Text>
+        <Text style={styles.title}>Tasks</Text>
+
+        <View style={styles.avatarContainer}>
+          <View style={styles.avatar}>
+            <Gravatar email={email} defaultImage="mm" />
+          </View>
+        </View>
+
+        <View style={styles.userInfo}>
+          <Text style={styles.name}>{name}</Text>
+          <Text style={styles.email}>{email}</Text>
+        </View>
       </View>
       <DrawerItemList {...props} />
     </DrawerContentScrollView>
--- Tasks/src/components/Drawer/styles.ts
+++ Tasks/src/components/Drawer/styles.ts
@@ -1,11 +1,42 @@
-import {StyleSheet} from 'react-native';
+import {Platform, StyleSheet} from 'react-native';
 
-//import commonStyles from '../../global/commonStyles';
+import commonStyles from '../../global/commonStyles';
 
 const styles = StyleSheet.create({
   header: {
     borderBottomWidth: 1,
     borderColor: '#DDD',
+    marginBottom: 5,
+  },
+  title: {
+    color: '#000',
+    fontFamily: commonStyles.fontFamily,
+    fontSize: 30,
+    paddingTop: Platform.OS === 'ios' ? 70 : 30,
+    padding: 10,
+  },
+  avatarContainer: {
+    flexDirection: 'row',
+  },
+  avatar: {
+    borderWidth: 2,
+    borderRadius: 30,
+    margin: 10,
+  },
+  userInfo: {
+    marginLeft: 10,
+  },
+  name: {
+    fontFamily: commonStyles.fontFamily,
+    color: commonStyles.colors.mainText,
+    fontSize: 20,
+    marginBottom: 5,
+  },
+  email: {
+    fontFamily: commonStyles.fontFamily,
+    color: commonStyles.colors.subText,
+    fontSize: 15,
+    marginBottom: 10,
   },
 });
 
