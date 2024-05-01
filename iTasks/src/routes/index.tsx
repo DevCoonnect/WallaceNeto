38c80bbb146ce0cd109c81a0fc9362cfe3bcdf4a
Wallace Neto <wallace.neto@outlook.com>
2024-05-01 19:21:08 -0300

    feat: adding splash screen

--- iTasks/src/routes/index.tsx
+++ iTasks/src/routes/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import BootSplash from "react-native-bootsplash";
 
 import AuthenticationRoutes from './authentication';
 import HomeRoutes from './home';
 
 const Routes: React.FC = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer onReady={() => BootSplash.hide({fade: true})}>
       <AuthenticationRoutes />
       {/* <HomeRoutes /> */}
     </NavigationContainer>
